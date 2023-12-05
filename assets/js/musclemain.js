6608daebda68bb137ae564957f44fc709e69cbb7
ramonlopezb <ramonlb54@gmail.com>
2023-12-05 12:43:59 -0300

    caracterisrticas de los musculos
    parametross de los musculos

--- assets/js/musclemain.js
+++ assets/js/musclemain.js
@@ -1,4 +1,7 @@
 console.log("hello wolrd");
+let EjerciciosMusculares = document.getElementById("ejerciciosMusculares");
+EjerciciosMusculares.innerHTML = "Muscle Exercises"
+
 let musculos = ['abdominals','abductors','biceps','calves','chest','forearms','glutes','hamstrings','lats','lower_back'];
 
 async function fetchdata(muscle){
@@ -10,3 +13,25 @@ async function fetchdata(muscle){
 	return data;	
 }
 fetchdata();
+
+async function tiposDeMusculo(){
+    for(i= 0; i < 3 ; i++){
+        let json = await fetchdata(musculos[i]);
+        console.log(json);
+        let h1 = document.getElementById(`muscle${i}`)
+        h1.innerHTML = json[0]["muscle"]
+        let h2 = document.getElementById(`name${i}`);
+        h2.innerHTML= json[0]["name"];
+        let claseDeEjercicio = document.getElementById(`type${i}`);
+        claseDeEjercicio.innerHTML=json[0] ["type"];
+        let equipoParaElEjercicio = document.getElementById(`equipment${i}`);
+        equipoParaElEjercicio.innerHTML = json[0] ["equipment"];
+        let complejidadDelEjercicio = document.getElementById(`difficulty${i}`);
+        complejidadDelEjercicio.innerHTML = json[0] ["difficulty"];
+        let ilustracionDelEjercicio = document.getElementById(`instructions${i}`);
+        ilustracionDelEjercicio.innerHTML = json[0]["instructions"];
+        
+    }
+}
+tiposDeMusculo();
+
